c4315aa527e3ae69c99c465136833b9d20311c4d
K3nK0 <uncommonmen@hotmail.fr>
2023-09-09 20:50:52 +0200

    add component navbar

--- src/layouts/management/Management.jsx
+++ src/layouts/management/Management.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import { useEffect, useState } from "react"
+import { collection, getDocs } from "firebase/firestore";
+
+import Employee from "./Employee"
+import {db} from "../../config/firebase"
 
 export default function Management() {
+
+  const [listEmployees, setListEmployees] = useState()
+
+  const getEmployees = async () => {
+    const querySnapshot = await getDocs(collection(db, "employees"));
+    const employees = querySnapshot.docs.map(doc => ({id: doc.id, ...doc.data()}))
+    // console.log("employées",employees);
+
+    setListEmployees(employees)
+    console.log("list employees",listEmployees);
+  }
+
+  useEffect(() => {
+    getEmployees()
+  }, [])
+  
+  console.log("list employees",listEmployees);
+
   return (
-    <div>
-      <h2>Gestion employés</h2>
-    </div>
+    <main>
+      <h2>Gestion des employés</h2>
+      <button className="btn-add-employee">Ajouter un employé</button>
+      {listEmployees !== undefined &&(
+        listEmployees.map(employee => (
+          <Employee id={employee.id} employee={employee} />
+        ))
+      )}
+    </main>
   )
 }
